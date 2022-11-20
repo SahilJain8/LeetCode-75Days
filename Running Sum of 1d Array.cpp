8491588cfb7b6d403f033e5d22eea580251f21a3
Sahil <68115080+SahilJain8@users.noreply.github.com>
2022-11-20 13:35:28 +0530

    Update Running Sum of 1d Array.cpp

--- Running Sum of 1d Array.cpp
+++ Running Sum of 1d Array.cpp
@@ -1,10 +1,18 @@
 class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
-       for( int i=1 ; i<nums.size() ; i++ )
+       for( int i=1 ; i<nums.size() ; i++ ) //Start from  1 
             nums[i] += nums[i-1] ;
         return nums ;
         
     }       
     
 };
+
+
+
+///Sample OutPut
+
+// Input: nums = [1,2,3,4]
+// Output: [1,3,6,10]
+// Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
